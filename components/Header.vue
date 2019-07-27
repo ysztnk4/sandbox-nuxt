c60376c004a8d306f63c3ab98ae201dfcafc4b66
ysztnk4 <ysztnk4@gmail.com>
2019-07-28 00:56:25 +0900

    feat: add Header and Footer

---
+++ components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="header">
+    <b><nuxt-link to="/">Hello Qiita with Nuxt.js</nuxt-link></b>
+  </div>
+</template>
+
+<style>
+.header {
+  left: 0;
+  top: 0;
+  width: 100%;
+  padding: 15px;
+  background-color: #59bb0c;
+  color: #fff;
+  font-size: 20px;
+  text-align: left;
+}
+
+.header a {
+  text-decoration: none;
+}
+
+.header a:link,
+.header a:visited {
+  color: #fff;
+}
+</style>
